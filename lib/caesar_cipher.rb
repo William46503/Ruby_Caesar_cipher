0d648aa3e8751d52ddb79f74b28f82b05555d90f
William <williamlu46503@outlook.jp>
2021-05-06 23:21:28 +0900

    Caesar_cipher_completed

---
+++ lib/caesar_cipher.rb
@@ -0,0 +1,6 @@
+require "caesar_cipher/version"
+
+module CaesarCipher
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/caesar_cipher_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe CaesarCipher do
+  it "has a version number" do
+    expect(CaesarCipher::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
